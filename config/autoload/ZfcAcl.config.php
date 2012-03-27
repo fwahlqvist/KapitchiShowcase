b3db2ddf6dde7c7ec1ed5c87a7ca223edd6e552c
Matus Zeman <matus.zeman@gmail.com>
2012-03-27 15:13:31 +0100

    showcase backup

---
+++ config/autoload/ZfcAcl.config.php
@@ -0,0 +1,49 @@
+<?php
+return array(
+    'ZfcAcl' => array(
+        'options' => array(
+            'enable_cache' => false,//enable static acl session cache 
+            'enable_guards' => array(
+                'route' => true,//enable route guard
+                'event' => true,//enable event guard
+            )
+        ),
+    ),
+    'di' => array(
+        'instance' => array(
+            'ZfcAcl\Model\Mapper\AclLoaderConfig' => array(
+                'parameters' => array(
+                    'config' => array(
+                        'resources' => array(
+                            'Route/Default' => null,//used by ZfcAcl\Guard\Route
+                        ),
+                        'roles' => array(
+                            'guest' => null,
+                            'auth' => null,
+                            'user' => 'auth',
+                            'admin' => 'user',
+                        ),
+
+                        'rules' => array(
+                            'allow' => array(
+                                'allow/default_route' => array(array('auth', 'guest'), 'Route/Default'),
+                            )
+                        )
+                    ),
+                ),
+            ),
+            'ZfcAcl\Model\Mapper\RouteResourceMapConfig' => array(
+                'parameters' => array(
+                    'config' => array(
+                        'default' => 'Route/Default',
+                        'child_map' => array(
+                            'default' => array(
+                                'default' => 'Route/Default',
+                            )
+                        )
+                    )
+                )
+            ),
+        )
+    ),
+);
