7a7e43699b8bd99a2451afa7562a0473917333b8
Matus Zeman <matus.zeman@gmail.com>
2012-04-02 20:38:43 +0100

    mysql stuff

--- dev/mysql_create.sql
+++ dev/mysql_create.sql
@@ -3,32 +3,16 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 19, 2012 at 07:34 PM
+-- Generation Time: Apr 02, 2012 at 07:37 PM
 -- Server version: 5.5.16
 -- PHP Version: 5.3.8
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
--- --------------------------------------------------------
-
 --
--- Table structure for table `auth_credential`
+-- Database: `creditors_drazobnik`
 --
-
-CREATE TABLE IF NOT EXISTS `auth_credential` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` text,
-  `password` text,
-  `identityId` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 -- --------------------------------------------------------
 
@@ -45,7 +29,7 @@ CREATE TABLE IF NOT EXISTS `contact` (
   `honorificSuffix` text,
   `middleName` text,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -86,7 +70,7 @@ CREATE TABLE IF NOT EXISTS `contact_identity` (
   `contactId` int(11) NOT NULL DEFAULT '0',
   `identityId` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -112,8 +96,39 @@ CREATE TABLE IF NOT EXISTS `identity` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `created` datetime DEFAULT NULL,
   `ownerId` int(11) DEFAULT NULL,
+  `roleId` text,
+  `updated` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `identity_auth_credential`
+--
+
+CREATE TABLE IF NOT EXISTS `identity_auth_credential` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` text,
+  `passwordHash` text,
+  `identityId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `identity_registration`
+--
+
+CREATE TABLE IF NOT EXISTS `identity_registration` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `identityId` int(11) DEFAULT NULL,
+  `data` text,
+  `requestIp` varchar(32) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -133,21 +148,4 @@ CREATE TABLE IF NOT EXISTS `location_address` (
   `region` text,
   `streetAddress` text,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `firstname` text,
-  `surname` text,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
